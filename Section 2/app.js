c8f96b447928a5edf3e8b73758cf7c24e1377a93
Waqas <w.ahmed@quentn.com>
2023-12-30 17:02:43 +0500

    section 2, chapter 25, Computed Properties

--- Section 2/app.js
+++ Section 2/app.js
@@ -10,20 +10,23 @@ const vm = Vue.createApp({
         }
     },
     methods : {
-      fullName() {
-          return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
-        },
       increment() {
           this.age++;
       },
       updateLastName(msg, event) {
-          console.log(msg);
+          //console.log(msg);
           this.lastName = event.target.value;
       },
       updateMiddleName(event) {
           this.middleName = event.target.value;
       }
     },
+   computed : {
+       fullName() {
+           console.log('fullname method computed property is called');
+           return `${this.firstName} ${this.middleName} ${this.lastName.toUpperCase()}`
+       },
+   }
 }).mount('#app');
 
 /*
