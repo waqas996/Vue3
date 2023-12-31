3092a657c75417285f5c7120c4080ae28e604fe2
Waqas <waqas996@gmail.com>
2023-12-31 20:05:29 +0500

    section 2, chapter 29(1), Conditional Rendering Starter

--- Section 2/app.js
+++ Section 2/app.js
@@ -1,20 +1,2 @@
 let vm = Vue.createApp({
-    data() {
-        return {
-            isPurple : false,
-            selectedColor : '',
-            size : 150,
-        }
-    },
-    computed : {
-        circle_classes() {
-            return {purple : this.isPurple};
-        },
-        circle_size() {
-            return [
-                { width: this.size + 'px', height: this.size + 'px', lineHeight: this.size + 'px' },
-                { transform: 'rotate(30deg)' }
-            ]
-        }
-    }
-}).mount('#app')
+}).mount('#app');
