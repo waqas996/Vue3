94bf6a2fda7d26125a63e9db07144f7be877c99c
Waqas <waqas996@gmail.com>
2023-12-31 20:02:40 +0500

    section 2, chapter 28, Binding Styles

--- Section 2/app.js
+++ Section 2/app.js
@@ -3,11 +3,18 @@ let vm = Vue.createApp({
         return {
             isPurple : false,
             selectedColor : '',
+            size : 150,
         }
     },
     computed : {
         circle_classes() {
             return {purple : this.isPurple};
+        },
+        circle_size() {
+            return [
+                { width: this.size + 'px', height: this.size + 'px', lineHeight: this.size + 'px' },
+                { transform: 'rotate(30deg)' }
+            ]
         }
     }
 }).mount('#app')
